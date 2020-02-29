21e84ad970a662fcb456decfd8b52c81dbddc23b
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2020-02-29 19:55:37 +0200

    updated buzzer logic

--- Smart_Bibrone.ino
+++ Smart_Bibrone.ino
@@ -8,8 +8,10 @@ OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 
-float celsius = 0;
 const int buzzer = 9;    // Buzzer to arduino pin 9
+float celsius = 0;
+char degree = 176; //ASCI value of Degree
+
 int state = 0;
 
 void setup(void)
@@ -18,68 +20,67 @@ void setup(void)
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   Serial.begin(9600);  
-  // Serial.begin(38400); // Default communication rate of the Bluetooth module
   
   sensors.begin();
-  
-  tone(buzzer, 1000, 500);
-  noTone(buzzer);
+
+  digitalWrite(buzzer, HIGH);
+  delay(500);
+  digitalWrite(buzzer, LOW);
+  delay(500);
 }
 
 void loop(void)
-{ 
-  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-  
-  celsius = sensors.getTempCByIndex(0);
-
+{
   if(Serial.available() > 0){    // Checks whether data is comming from the serial port
     state = Serial.read();      // Reads the data from the serial port
     }
+    
   if (state == '0') {
     digitalWrite(ledPin, LOW);     // Turn LED OFF
     Serial.println("LED: OFF");    // Send back, to the phone, the String "LED: ON"
     state = 0;
     }
+    
   else if (state == '1') {
     digitalWrite(ledPin, HIGH);
     Serial.println("LED: ON");
-    state = 0;
-    } 
-  
-  // Check if reading was successful
-  if(celsius != DEVICE_DISCONNECTED_C) 
-  {
+    
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    celsius = sensors.getTempCByIndex(0);
+    
     Serial.print("Temperature is: ");
-    Serial.print(celsius);  
+    Serial.print(celsius);
+    Serial.print(degree);
     Serial.println(" C ");
-  } 
-  else
-  {
-    Serial.println("Error: Could not read temperature data");
-  }
-  
+
   // Check the right range of temperature
   if (celsius >= 30 && celsius <= 38)
   {
     Serial.println("The milk is ready");
+    delay(500);
   }
   else if(celsius > 38)
   {
-    Serial.println("The milk is too hot, it needs to be cooled");
-    tone(buzzer, 1000, 500);
-    noTone(buzzer);
+    Serial.println("The milk is too hot, you should let it cool");
+    digitalWrite(buzzer, HIGH);
     delay(500);
-    tone(buzzer, 1000, 500);
-    noTone(buzzer);
+    digitalWrite(buzzer, LOW);
+    delay(500);
+    digitalWrite(buzzer, HIGH);
+    delay(500);
+    digitalWrite(buzzer, LOW);
     delay(500);
   }
    else if(celsius < 30)
   {
-    Serial.println("The milk should be heated");
-    tone(buzzer, 1000, 1500);
-    noTone(buzzer);
+    Serial.println("The milk is too cold, you should heat it");
+    digitalWrite(buzzer, HIGH);
+    delay(1500);
+    digitalWrite(buzzer, LOW);
     delay(500);
   }
+  
+    state = 0;
+    } 
+
 }
